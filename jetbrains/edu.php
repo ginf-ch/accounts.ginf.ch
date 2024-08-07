04615efac72d5f229675bdbb4e8ebf38d3923abf
Oliver Probst <pro@kswe.ch>
2024-08-07 20:18:48 +0200

    feat: update description wrt version 2024

--- jetbrains/edu.php
+++ jetbrains/edu.php
@@ -1,24 +1,30 @@
 <?php
 $steps = [
-    'Rufen Sie die Website von <a target="_blank" href="https://www.jetbrains.com">JetBrains</a> auf. Klicken Sie dort mit der Maus auf <span class="icon"><i class="fa-solid fa-user"></i></span> (<q>JetBrains Account</q>).',
+    'Falls noch nicht erledigt: Starten Sie <b>PyCharm</b> und klicken Sie auf <q>Register...</q>.',
     'Suchen Sie den <q>Not registered yet?</q>-Bereich und geben Sie <b class="is-uppercase">Ihre E-Mail-Adresse</b> der <b class="is-uppercase">KSWE</b> ein. Klicken Sie dann auf <q>Sign up</q>.',
     'Rufen Sie Ihre <b>KSWE-E-Mails</b> ab.',
-    'Öffnen Sie die E-Mail mit dem Absender <q>JetBrains Account</q>. Klicken Sie darin auf den Link <q>Confirm your account</q>.',
+    'Öffnen Sie die <b>E-Mail</b> mit dem Absender <q>JetBrains Account</q>. Klicken Sie darin auf den Link <q>Confirm your account</q>.',
     'Eine Webseite mit einem Anmeldeformular öffnet sich. Geben Sie <b class="is-uppercase">Ihren Vor- und Nachnamen</b> sowie einen <b>Benutzernamen</b> (z. B. E-Mail-Adresse) ein. Wählen Sie ein <b>Passwort</b>, das Sie sich gut merken können (ggf. notieren). Fahren Sie dann mit dem nächsten Schritt für den Rest des Formulars fort...',
-    'Wiederholen Sie das Passwort und wählen Sie nur die erste Checkbox aus. Klicken Sie abschliessend auf <q>Submit</q>.',
-    'Der erste Teil der Registrierung ist nun abgeschlossen. Um eine kostenlose Lizenz für Bildungseinrichtungen zu erhalten, klicken Sie auf den Link <q>Apply for a free student or teacher license</q>.',
-    'Scrollen Sie nach unten...',
-    '...bis Sie den Link <q>Apply now</q> sehen. Klicken Sie auf diesen Link.',
+    'Wiederholen Sie das <b>Passwort</b> und wählen Sie nur die <b>erste Checkbox</b> aus. Klicken Sie abschliessend auf <q>Submit</q>.',
+    'Der erste Teil der Registrierung ist nun abgeschlossen. Sie müssen jetzt eine <b>kostenlose Lizenz für Bildungseinrichtungen</b> beziehen. Klicken Sie auf den Link <q>Apply for a free student or teacher license</q>.',
+    'Scrollen Sie nach <b>unten</b>...',
+    '...bis Sie den Button <q>Apply now</q> sehen. <b>Klicken</b> Sie auf diesen Button.',
     'Füllen Sie das Formular wahrheitsgemäss aus. Verwenden Sie wieder <b class="is-uppercase">Ihre KSWE-E-Mail-Adresse</b>...',
     '...und Ihren <b>Vor- und Nachnamen</b>. Wählen Sie <b>Schweiz</b> aus und markieren Sie <b>nur</b> die mittlere Checkbox.',
-    'Beantragen Sie schliesslich die Lizenz, indem Sie auf den Link klicken.',
+    'Beantragen Sie schliesslich die Lizenz, indem Sie auf den <b>Button</b> <q>Kostenlose Produkte beantragen</q> klicken.',
     'Rufen Sie wieder Ihre <b>KSWE-E-Mails</b> ab.',
-    'Öffnen Sie nun die E-Mail mit dem Titel <q>JetBrains Educational Pack</q>. Klicken Sie darin auf den Link.',
-    'Eine Webseite mit öffnet sich. Bestätigen Sie mit einem Klick auf <q>Get stared to use</q>.',
-    'Scrollen Sie nach unten...',
-    '...bis Sie der Link <q>I Accept</q> anklickbar wird (blau). Klicken Sie auf diesen Link.',
-    'Es öffnet sich eine Webseite mit einem Login-Formular. Geben Sie Ihre Daten ein und klicken Sie auf <q>Sign In</q>.',
-    'Die Anleittung ist abgeschlossen. Sie haben nun die kostenlose Lizenz für Bildungseinrichtungen für ein Jahr erhalten.'
+    'Öffnen Sie nun die <b>E-Mail</b> mit dem Titel <q>JetBrains Educational Pack</q>. Klicken Sie darin auf den ersten <b>Link</b>.',
+    'Eine Webseite mit öffnet sich. Bestätigen Sie mit einem Klick auf den <b>Button</b> <q>Get stared to use</q>.',
+    'Scrollen Sie nach <b>unten</b>...',
+    '...bis Sie der <b>Button</b> <q>I Accept</q> anklickbar wird (blau). Klicken Sie dann auf diesen Button.',
+    'Es öffnet sich eine Webseite mit einem <b>Login-Formular</b>. Geben Sie Ihre Daten ein und klicken Sie auf den <b>Button</b> <q>Sign In</q>.',
+    'Sie haben nun die kostenlose Lizenz für Bildungseinrichtungen für ein Jahr erhalten. Kehren Sie jetzt zu <b>PyCharm</b> zurück.',
+    'Klicken Sie im <b>PyCharm</b>-Fenster auf <q>Log In to JetBrains Account...</q>.',
+    'Es öffnet sich eine <b>Website</b>. Loggen Sie sich gegebenenfalls nochmals ein. Sonst können Sie direkt zum nächsten Schritt.',
+    'Wenn Sie diese <b>Website</b> sehen, dann hat die Verbindung zwischen <b>PyCharm</b> und dem JetBrains Account erfolgreich geklappt. Kehren Sie dann zu PyCharm zurück.',
+    'Klicken Sie nun auf den <b>Button</b> <q>Activate with JetBrains Account</q>.',
+    'Klicken Sie nun auf den <b>Button</b> <q>Continue</q>.',
+    'Erstellen Sie <b class="is-uppercase">kein</b> Projekt (auch kein neues Projekt), sondern <b>schliessen</b> Sie PyCharm. Ihre <b>Lehrperson</b> wird die weiteren Einstellungen mit Ihnen im Unterricht vornehmen.'
 ];
 $numberOfSteps = count($steps);
 include("hinweis.php");
